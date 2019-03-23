1280918f7273aeee0c90945d65969116a56d06f1
sml156 <sml156@hotmail.com>
2019-03-23 05:19:04 -0400

    Update test_white.sh

--- test_white.sh
+++ test_white.sh
@@ -21,4 +21,4 @@ curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/whitelist
 
 sudo wget -qO - https://v.firebog.net/hosts/lists.php?type=tick | sudo tee /etc/pihole/personal-adlists.list
 
-cat /etc/pihole/personal-adlists.list <(wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross 2> /dev/null) | sudo tee /etc/pihole/adlists.list
+cat /etc/pihole/personal-adlists.list < wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross 2> /dev/null | sudo tee /etc/pihole/adlists.list
