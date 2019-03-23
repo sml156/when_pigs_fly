374dfb29aa5f64ab5777304314dc2a738f7e3905
sml156 <sml156@hotmail.com>
2019-03-23 04:34:57 -0400

    Update test_white.sh

--- test_white.sh
+++ test_white.sh
@@ -3,22 +3,16 @@
 # cp -f /etc/.pihole/adlists.default /etc/pihole/adlists.list && pihole -g
 
 # ##############################################
-# Custom blacklist
+# My Custom blacklist from Github
 # ##############################################
 
-#pihole -b cdn.adfront.org tag.medialytics.com med.heyzap.com notify.bugsnag.com rt.applovin.com ns.zdbb.net omaze.com traffic.adxprts.com ads.exoclick.com adclickservice.com 2468.go2cloud.org d1lp05q4sghme9.cloudfront.net 4-edge-chat.facebook.com pleasedontslaymy.download cdn11.pleasedontslaymy.download engine.phn.doublepimp.com media.trafficjunky.net fbcdn-profile-a.akamaihd.net cf2.vuze.com tlootas.org badtopwitch.work eventtracking.omarsys.com almoraffiliates-api.omarsys.com www.sunnyplayer.com cdn.engine.spotscenered.info sunnyplayer-frontapi.cherrytech.com mh.tlootas.org spotscenered.info 
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/blacklist.txt > /etc/pihole/blacklist.txt
 
 # ##############################################
-# Whitelist of domains (twitter, spotify, ...)
+# # add my list from github
 # ##############################################
 
-# Clean out - to be sure
-# echo "" | sudo tee /etc/pihole/whitelist.txt > /dev/null
-# pihole -g
-
-#pihole -w raw.githubusercontent.com mirror1.malwaredomains.com sysctl.org zeustracker.abuse.ch s3.amazonaws.com hosts-file.net spclient.wg.spotify.com udm.scorecardresearch.com analytics.twitter.com adaway.org v.firebog.net mirror.cedia.org.ec www.malwaredomainlist.com bitbucket.org openphish.com ransomwaretracker.abuse.ch github.com thepiratebay.org apple.com appleid.apple.com pornhub.com opensubtitles.org angryip.org imgur.com prod.imgur.map.fastlylb.net m.imgur.com 
-# add my list from github
-echo "https://raw.githubusercontent.com/sml156/when_pigs_fly/master/whitelist.txt" | sudo tee /etc/pihole/whitelist.txt > /dev/null
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/whitelist.txt > /etc/pihole/whitelist.txt
 
 # ##############################################
 # Pi-Hole's block lists
