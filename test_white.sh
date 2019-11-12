8fa533ba97b596dfa85b531222b43cb51b5066dd
sml156 <sml156@hotmail.com>
2019-11-12 00:43:49 -0500

    Update test_white.sh

--- test_white.sh
+++ test_white.sh
@@ -12,50 +12,37 @@
 # My blacklist on github
 # ##############################################
 
-curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/blacklist.txt | tee /etc/pihole/blacklist.txt 1>/dev/null && echo "OK Blacklist Copied" || echo "Blacklist Failed"
-curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/black.list | tee /etc/pihole/black.list 1>/dev/null && echo "OK Blacklist2 Copied" || echo "Blacklist2 Failed"
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/blacklist.txt | tee /etc/pihole/blacklist.txt 1>/dev/null && echo "OK My Blacklist Copied" || echo "My Blacklist Failed"
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/black.list | tee /etc/pihole/black.list 1>/dev/null && echo "OK My Blacklist2 Copied" || echo "My Blacklist2 Failed"
 # ##############################################
 # My Whitelist on github
 # ##############################################
 
-curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/whitelist.txt | tee /etc/pihole/whitelist.txt 1>/dev/null && echo "OK Whitelist copied" || echo "Whitelist Failed"
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/whitelist.txt | tee /etc/pihole/whitelist.txt 1>/dev/null && echo "OK My Whitelist copied" || echo "My Whitelist Failed"
 
 # ##############################################
 # My regex on github
 # ##############################################
 
-curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/regex.list | tee /etc/pihole/regex.list 1>/dev/null && echo OK "Regex Copied" || echo "Regex Failed"
+curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/regex.list | tee /etc/pihole/regex.list 1>/dev/null && echo "OK My Regex Copied" || echo "My Regex Failed"
 
 # ##############################################
 # Pi-Hole's block lists from https://wally3k.github.io
-# Remove comment from only one, Do not use all
 # ##############################################
 
-curl -sS https://v.firebog.net/hosts/lists.php?type=nocross | sudo tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally" || echo "wally Blacklist Failed"
+# Only pick one of these
+### curl -sS https://v.firebog.net/hosts/lists.php?type=tick | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
+curl -sS https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
+### curl -sS https://v.firebog.net/hosts/lists.php?type=all | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
 
+# Appened some new lists (NOTE: tee -a) from https://www.github.developerdan.com/hosts/ -2019-11-11 04:50:05
+# Ads & Tracking -Updated semi regular-
+curl -sS https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended1 Downloaded"  || echo "My apended1 Failed Download"
+# Tracking Aggressive -Updated semi regular-
+curl -sS https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended2 Downloaded"  || echo "My apended2 Failed Download"
+# AMP Hosts -Updated semi regular-
+curl -sS https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended3 Downloaded"  || echo "My apended3 Failed Download"
 
-### wget -qO - https://v.firebog.net/hosts/lists.php?type=tick | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
-### echo "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee /etc/pihole/adlists.list > /dev/null
-### wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
-### wget -qO - https://v.firebog.net/hosts/lists.php?type=all | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
-
-# something wrong here worked fine yesterday
-# Appened some new lists from https://www.github.developerdan.com/hosts/ -2019-11-11 04:50:05
-# Ads & Tracking
-echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended1 Downloaded"  || echo "apended1 Failed Download"
-# Tracking Aggressive
-echo "https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended2 Downloaded"  || echo "apended2 Failed Download"
-# AMP Hosts
-echo "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended3 Downloaded"  || echo "apended3 Failed Download"
-
-
-
-
-# ##############################################
-# Template to add more domains NOTE: Be aware whether you want to append EG:  -a  or not append
-# echo "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-# echo "https://mirror1.malwaredomains.com/files/justdomains" | sudo tee -a /etc/pihole/adlists.list > /dev/null
-# ##############################################
 
 # ##############################################
 # Update pihole
