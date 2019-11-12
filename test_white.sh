765fdc257ad9e774a68338a65eb008c8b394857d
sml156 <sml156@hotmail.com>
2019-11-11 19:53:48 -0500

    Update test_white.sh

--- test_white.sh
+++ test_white.sh
@@ -32,24 +32,25 @@ curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/regex.lis
 # ##############################################
 
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=tick | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
-wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
+echo "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee -a /etc/pihole/adlists.list > /dev/null
+# wget -qO - https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
 #wget -qO - https://v.firebog.net/hosts/lists.php?type=all | tee /etc/pihole/adlists.list 1>/dev/null && echo OK "wally3k Downloaded"  || echo "wally3k Failed Download"
 
 # something wrong here worked fine yesterday
 # Appened some new lists from https://www.github.developerdan.com/hosts/ -2019-11-11 04:50:05
 # Ads & Tracking
-# echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended1 Downloaded"  || echo "apended1 Failed Download"
+echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended1 Downloaded"  || echo "apended1 Failed Download"
 # Tracking Aggressive
-# echo "https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended2 Downloaded"  || echo "apended2 Failed Download"
+echo "https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended2 Downloaded"  || echo "apended2 Failed Download"
 # AMP Hosts
-# echo "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended3 Downloaded"  || echo "apended3 Failed Download"
+echo "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt" | sudo tee -a /etc/pihole/adlists.list > /dev/null && echo OK "apended3 Downloaded"  || echo "apended3 Failed Download"
 
 
 
 
 # ##############################################
 # Template to add more domains NOTE: Be aware whether you want to append EG:  -a  or not append
-# echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" | sudo tee -a /etc/pihole/adlists.list > /dev/null
+# echo "https://v.firebog.net/hosts/lists.php?type=nocross" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 # echo "https://mirror1.malwaredomains.com/files/justdomains" | sudo tee -a /etc/pihole/adlists.list > /dev/null
 # ##############################################
 
