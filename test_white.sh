337c0ffa86faf866f42051256987d241f17cd45d
sml156 <sml156@hotmail.com>
2020-04-08 15:14:13 -0400

    Update test_white.sh

--- test_white.sh
+++ test_white.sh
@@ -32,18 +32,19 @@ curl -sS https://raw.githubusercontent.com/sml156/when_pigs_fly/master/regex.lis
 # Pi-Hole's block lists from https://wally3k.github.io
 # ##############################################
 
+echo " https://raw.githubusercontent.com/sml156/when_pigs_fly/master/blacklist_sites | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK My Github Downloaded"  || echo "My Github Failed"
 # Only pick one of these
 ### curl -sS https://v.firebog.net/hosts/lists.php?type=tick | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
-curl -sS https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
+# curl -sS https://v.firebog.net/hosts/lists.php?type=nocross | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
 ### curl -sS https://v.firebog.net/hosts/lists.php?type=all | tee /etc/pihole/adlists.list 1>/dev/null && echo "OK wally3k Downloaded"  || echo "wally3k Failed"
 
 # Appened some new lists (NOTE: tee -a) from https://www.github.developerdan.com/hosts/ -2019-11-11 04:50:05
 # Ads & Tracking -Updated semi regular-
-echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended1 Downloaded"  || echo "My apended1 Failed Download"
+# echo "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended1 Downloaded"  || echo "My apended1 Failed Download"
 # Tracking Aggressive -Updated semi regular-
-echo "https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended2 Downloaded"  || echo "My apended2 Failed Download"
+# echo "https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended2 Downloaded"  || echo "My apended2 Failed Download"
 # AMP Hosts -Updated semi regular-
-echo "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended3 Downloaded"  || echo "My apended3 Failed Download"
+# echo "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt" | tee -a /etc/pihole/adlists.list > /dev/null && echo "OK My apended3 Downloaded"  || echo "My apended3 Failed Download"
 
 
 # ##############################################
